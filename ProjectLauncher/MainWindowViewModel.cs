9951d98a4e5408f7cdc67898f964dda4c3986372
Jason L. Walker <jasonlanewalker@gmail.com>
2025-04-06 19:36:31 -0500

    Added first half of project view

--- ProjectLauncher/MainWindowViewModel.cs
+++ ProjectLauncher/MainWindowViewModel.cs
@@ -1,11 +1,21 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
+using System.Collections.ObjectModel;
 
 namespace ProjectLauncher
 {
     internal partial class MainWindowViewModel : ObservableObject
     {
+        public ObservableCollection<ProjectDisplayItem> ProjectDisplayItems { get; set; } = [];
+
+        public MainWindowViewModel()
+        {
+            // testing
+            ProjectDisplayItems.Add(new("Project Launcher", true));
+            ProjectDisplayItems.Add(new("Expense Tracker", true));
+        }
+
         [RelayCommand]
         public static void OnApplicationClosed()
         {
@@ -19,4 +29,6 @@ namespace ProjectLauncher
         }
 
     }
+
+    internal record ProjectDisplayItem(string ProjectName, bool IsActive);
 }
